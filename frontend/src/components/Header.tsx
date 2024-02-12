9024a9a2821fbf65f91459a54d19b56dddd2abc7
srisurya-m <srisurya.makkapati@iiitb.ac.in>
2024-02-12 09:14:30 +0530

    admin frontend setup

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { FaSearch, FaShoppingBag, FaSignInAlt } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+const user={_id:"ifrwo"};
+
+const Header = () => {
+  return (
+    <nav>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/search"}><FaSearch /></Link>
+      <Link to={"/cart"}><FaShoppingBag /></Link>
+
+      {
+        user?._id ? 
+        <>
+        
+        </> :
+        <Link to={"/login"}><FaSignInAlt /></Link>
+      }
+    </nav>
+  )
+}
+
+export default Header
