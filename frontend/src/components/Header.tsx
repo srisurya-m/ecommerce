494584c698204b5dfed84570cbb07d1488e57d3c
srisurya-m <srisurya.makkapati@iiitb.ac.in>
2024-02-12 12:17:50 +0530

    header ui completed

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,24 +1,63 @@
-import { FaSearch, FaShoppingBag, FaSignInAlt } from "react-icons/fa"
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const user={_id:"ifrwo"};
+const user = { _id: "ifrwo", role: "admin" };
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const logoutHandler = () => {
+    setIsOpen(false);
+  };
   return (
-    <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/search"}><FaSearch /></Link>
-      <Link to={"/cart"}><FaShoppingBag /></Link>
+    <nav className="header">
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+        Home
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
 
-      {
-        user?._id ? 
+      {user?._id ? (
         <>
-        
-        </> :
-        <Link to={"/login"}><FaSignInAlt /></Link>
-      }
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user.role === "admin" && (
+                <>
+                  <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+                    Admin Dashboard{" "}
+                  </Link>
+                </>
+              )}
+              <Link onClick={() => setIsOpen(false)} to="/orders">
+                Orders
+              </Link>
+              <button onClick={logoutHandler}>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
