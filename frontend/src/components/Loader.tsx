8eb0b4b45fc92826f8d61eb9afe26a516dce1f35
srisurya-m <srisurya.makkapati@iiitb.ac.in>
2024-05-04 23:25:03 +0530

    deployment-changes

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,19 +1,19 @@
-
-
 const Loader = () => {
   return (
-    <div>
-      Loading...
+    <section className="loader">
+      <div></div>
+    </section>
+  );
+};
+
+export const SkeletonLoader = () => {
+  return (
+    <div className="skeleton-loader">
+      <div className="skeleton-shape"></div>
+      <div className="skeleton-shape"></div>
+      <div className="skeleton-shape"></div>
     </div>
-  )
-}
+  );
+};
 
-export const SkeletonLoader = ()=>{
-  return <div className="skeleton-loader">
-    <div className="skeleton-shape"></div>
-    <div className="skeleton-shape"></div>
-    <div className="skeleton-shape"></div>
-  </div>
-}
-
-export default Loader
+export default Loader;
