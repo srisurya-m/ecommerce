b94baa58d736d0daf96b6f6fd378320c96843b46
srisurya-m <srisurya.makkapati@iiitb.ac.in>
2024-10-24 14:54:19 +0530

    update-completed user reviews functionality next to work on caching-4

--- frontend/src/pages/admin/products.tsx
+++ frontend/src/pages/admin/products.tsx
@@ -1,16 +1,15 @@
 import { ReactElement, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { FaPlus } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Column } from "react-table";
 import AdminSidebar from "../../components/admin/AdminSidebar";
 import TableHOC from "../../components/admin/TableHOC";
-import { useAllProductsQuery } from "../../redux/api/productApi";
-import { server } from "../../redux/store";
-import toast from "react-hot-toast";
-import { CustomError } from "../../types/apiTypes";
-import { useSelector } from "react-redux";
-import { userReducerInitialState } from "../../types/reducerTypes";
 import { SkeletonLoader } from "../../components/Loader";
+import { useAllProductsQuery } from "../../redux/api/productApi";
+import { CustomError } from "../../types/apiTypes";
+import { userReducerInitialState } from "../../types/reducerTypes";
 
 interface DataType {
   photo: ReactElement;
